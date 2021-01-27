0c765c752981dd0dc1d43ce3b0509e1a213d6804
Your Name <mashour365@gmail.com>
2021-01-28 00:54:45 +0200

    Added new firebase acc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:nav_router/nav_router.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -13,7 +14,10 @@ import 'Themes/themes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp();
+  LocalAuthentication auth = LocalAuthentication();
+  List<BiometricType> availableBiometrics = await auth.getAvailableBiometrics();
+  print(availableBiometrics);
   runApp(
     Providers(
       child: MaterialApp(
