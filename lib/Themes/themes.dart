d72a0f12ee40f8da9d61eae7c2c609cceca1a1c0
Your Name <mashour365@gmail.com>
2021-01-28 16:15:53 +0200

    UI fixes

--- lib/Themes/themes.dart
+++ lib/Themes/themes.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 ///Theme used in application.
 ThemeData themes = ThemeData(
-  primaryColor: const Color(0xff303030),
+  primaryColor: const Color(0xff89cff0),
   accentColor: const Color(0xff363A44),
-  brightness: Brightness.dark,
+  brightness: Brightness.light,
   iconTheme: IconThemeData(color: Colors.purple[300]),
-  scaffoldBackgroundColor: const Color(0xff303030),
+  scaffoldBackgroundColor: const Color(0xff89cff0),
   textTheme: TextTheme(
     button: TextStyle(color: Colors.red),
   ),
